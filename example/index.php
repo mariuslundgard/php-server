a485794f0dfb4ca3c3b755a2a4a91fb0dac546b8
Marius Lundgård <marius.lundgard@gmail.com>
2014-08-07 21:43:19 +0200

    update example

--- example/index.php
+++ example/index.php
@@ -4,14 +4,27 @@ putenv('DEBUG=1');
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = new Server\Stack();
+use Server\Module as App;
+
+class CustomMiddleware extends Server\Layer
+{
+    public function call(Server\Request $req = null, Server\Error $err = null)
+    {
+        $res = parent::call($req, $err);
+		$res->write('<h1>Hello, world!</h1>');
+		return $res;
+    }
+}
+
+$app = new App();
 
 $app->employ([
-	'class' => 'Gzip'
+    'class' => 'CustomMiddleware',
 ]);
 
-$res = $app->call(new Server\Request($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']));
+$res = $app->call(new Server\Request(
+    $_SERVER['REQUEST_METHOD'],
+    $_SERVER['REQUEST_URI']
+));
 
-$res->send();
-
-d($res);
+$res->send(); // outputs: <h1>Hello, world!</h1>
