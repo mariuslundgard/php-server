faaa4502413e07fd900e9676c7d98a898791a8ef
Marius Lundgård <marius.lundgard@gmail.com>
2014-08-09 11:50:01 +0200

    add getter tests

--- tests/Server/ResponseTest.php
+++ tests/Server/ResponseTest.php
@@ -13,6 +13,28 @@ class ResponseTest extends Base
         $this->assertInstanceOf('Server\Response', $res);
     }
 
+    public function testGetters()
+    {
+        $res = new Response(new Request('GET', '/test'));
+
+        // $this->assertInstanceOf('Server\Response', $res);
+        $this->assertTrue(is_array($res->headers));
+        $this->assertInstanceOf('Util\Dictionary', $res->data);
+        $this->assertEquals('text/html', $res->type);
+    }
+
+    public function testSetType()
+    {
+        $res = new Response(new Request('GET', '/test'));
+
+        // $this->assertInstanceOf('Server\Response', $res);
+        // $this->assertTrue(is_array($res->headers));
+        // $this->assertInstanceOf('Util\Dictionary', $res->data);
+        $res->type = 'application/json';
+
+        $this->assertEquals('application/json', $res->type);
+    }
+
     public function testWrite()
     {
         $res = new Response(new Request('GET', '/test'));
