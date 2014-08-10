1cff1ad576f990a7cfab744281e8cb40bda170fe
Marius Lundgård <marius.lundgard@gmail.com>
2014-08-10 13:50:34 +0200

    add staticUrl() test

--- tests/Server/ApplicationTest.php
+++ tests/Server/ApplicationTest.php
@@ -1,1 +1,28 @@
-ApplicationTest.php
+<?php
+
+namespace Server;
+
+use PHPUnit_Framework_TestCase as Base;
+
+class ApplicationTest extends Base
+{
+    public function testCreate()
+    {
+        $app = Application::create();
+
+        $this->assertInstanceOf('Server\Application', $app);
+    }
+
+    public function testStaticUrl()
+    {
+        $app = Application::create(null, array(
+            'baseStaticUrl' => 'http://localhost/'
+        ));
+
+        $this->assertEquals('http://localhost/test.css', $app->staticUrl('test.css'));
+
+        $app2 = Application::create();
+
+        $this->assertEquals('/test.css', $app2->staticUrl('test.css'));
+    }
+}
